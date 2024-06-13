f9798f0439239b41df5ba9d123f276e55fa05f9f
Daniel Lewis <daniel.lewis@plailabs.com>
2024-06-12 23:18:58 -0600

    Fixed mouse and trackpad zoom behavior as per PR 445 to Javi

--- src/litegraph.js
+++ src/litegraph.js
@@ -6927,11 +6927,7 @@ LGraphNode.prototype.executeAction = function(action)
 
         var scale = this.ds.scale;
 
-        if (delta > 0) {
-            scale *= 1.1;
-        } else if (delta < 0) {
-            scale *= 1 / 1.1;
-        }
+        scale *= Math.pow(1.1, delta * 0.01);
 
         // this.setZoom( scale, [ e.clientX, e.clientY ] );
         this.ds.changeScale(scale, [e.clientX, e.clientY]);
