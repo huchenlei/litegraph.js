b032aa3409bd81eff54dd04f9719c56be62a404b
Daniel Lewis <daniel.lewis@plailabs.com>
2024-06-13 00:25:39 -0600

    Fixed width and height error in contextmenu

--- src/litegraph.js
+++ src/litegraph.js
@@ -13568,8 +13568,6 @@ LGraphNode.prototype.executeAction = function(action)
         if (options.className) {
             root.className += " " + options.className;
         }
-        root.style.minWidth = 100;
-        root.style.minHeight = 100;
         root.style.pointerEvents = "none";
         setTimeout(function() {
             root.style.pointerEvents = "auto";
